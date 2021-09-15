d9050554d3dbd4b682e4d67de24c1228a1e55efb
Edgar Cirilo <edcirilo_9216@live.com.mx>
2021-09-14 23:34:29 -0500

    Remove wildcards and adding junit console

--- src/test/java/com/ecirilo/service/impl/InventoryServiceImplTest.java
+++ src/test/java/com/ecirilo/service/impl/InventoryServiceImplTest.java
@@ -18,7 +18,8 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
@@ -60,7 +61,7 @@ class InventoryServiceImplTest {
     }
 
     @Test
-    @DisplayName("Should throw an IllegalArgumentException")
+    @DisplayName("Should throw an IllegalArgumentException if itemDTO is null")
     void createItem_NullParam_Fail() {
         Throwable throwable = catchThrowable(() -> inventoryService.createItem(null));
 
